4c712d4fc35d4741cf5943a21204c1b352d26ea4
cake? <willsonputra17@gmail.com>
2024-04-11 22:36:10 +0700

    asd
    asd

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from tkinter import *
 import webbrowser
 from tkinter import ttk
+import random
+
+anime_list = ["a"]#put your music link
+
+def anime():
+    x = 0
+    for x in range(0, len(anime_list)):
+        return anime_list[x]
 
 
-def start_music():
-    webbrowser.open("")#put your music list
-    # select music
-
-def party_music():
-    webbrowser.open("")#put your music list
+def start_music(url):
+    webbrowser.open(url)
 
 def quit_application():
     root.destroy()
@@ -20,11 +24,11 @@ label = ttk.Label(root, text="hewo im emu")
 label.grid(column=5, row=0)
 
 # Create and place the button 
-button1 = ttk.Button(root, text="anime music", command=start_music)
+button1 = ttk.Button(root, text="anime music", command=lambda: start_music(anime()))
 button1.grid(column=5, row=1)
 
 # Create and place the button 
-button2 = ttk.Button(root, text="party music", command=start_music)
+button2 = ttk.Button(root, text="party music", command=lambda: start_music("Put your party music URL here"))
 button2.grid(column=5, row=2)
 
 # Create and place the button 
@@ -32,3 +36,4 @@ button3 = ttk.Button(root, text="Quit", command=quit_application)
 button3.grid(column=5, row=5)
 
 root.mainloop()
+
