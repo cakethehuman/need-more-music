717e19b53009adda88ad7d5930a56167245f4ecc
cake? <willsonputra17@gmail.com>
2024-04-11 22:19:34 +0700

    update for list
    cool

--- main.py
+++ main.py
@@ -4,30 +4,31 @@ from tkinter import ttk
 
 
 def start_music():
-    webbrowser.open("https://www.youtube.com/watch?v=E5Jy_h1eHzY&list=RDAqI97zHMoQw&index=2")
+    webbrowser.open("")#put your music list
     # select music
 
 def party_music():
-    webbrowser.open("https://www.youtube.com/watch?v=E5Jy_h1eHzY&list=RDAqI97zHMoQw&index=2")
+    webbrowser.open("")#put your music list
 
 def quit_application():
     root.destroy()
 
 root = Tk()
-root.geometry("600x400")
 
 # Create and place the label 
 label = ttk.Label(root, text="hewo im emu")
 label.grid(column=5, row=0)
 
-button = ttk.Button(root, text="music", command=start_music)
-button.grid(column=5, row=1)
-
-button = ttk.Button(root, text="black people music", command=start_music)
-button.grid(column=5, row=1)
+# Create and place the button 
+button1 = ttk.Button(root, text="anime music", command=start_music)
+button1.grid(column=5, row=1)
 
 # Create and place the button 
-button = ttk.Button(root, text="Quit", command=quit_application)
-button.grid(column=5, row=2)
+button2 = ttk.Button(root, text="party music", command=start_music)
+button2.grid(column=5, row=2)
+
+# Create and place the button 
+button3 = ttk.Button(root, text="Quit", command=quit_application)
+button3.grid(column=5, row=5)
 
 root.mainloop()
